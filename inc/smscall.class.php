95c73535bb9dae5fa6806a8bf5aa3fe43ccdbf96
mostafa.s1990 <mostafa.s1990@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-01 11:34:59 +0000

    commplate files.
    git-svn-id: https://plugins.svn.wordpress.org/wp-sms/tags/1.9.10@689981 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/smscall.class.php
@@ -0,0 +1,35 @@
+<?php
+	class smscall
+	{
+		private $wsdl_link = "http://webservice.smscall.ir/index.php?wsdl";
+		public $tariff = "http://www.smscall.ir/?page_id=63";
+		public $unitrial = false;
+		public $unit;
+		public $flash = "enable";
+		public $user;
+		public $pass;
+		public $from;
+		public $to;
+		public $msg;
+		public $isflash = false;
+
+		function __construct() {
+		
+			ini_set("soap.wsdl_cache_enabled", "0");
+		}
+
+		function send_sms() {
+			
+			$client = new SoapClient($this->wsdl_link);
+			
+			return $client->Send_Group_SMS($this->user, $this->pass, implode(',', $this->to), $this->msg, $this->from, 1);
+		}
+
+		function get_credit() {
+		
+			$client = new SoapClient($this->wsdl_link);
+			
+			return $client->CREDIT_LINESMS($this->user, $this->pass, $this->from);
+		}
+	}
+?>
