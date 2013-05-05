2dd55fcdc1baa75446fb95596573a378b2c5d8c6
mostafa.s1990 <mostafa.s1990@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-05 14:15:36 +0000

    commplate files.
    git-svn-id: https://plugins.svn.wordpress.org/wp-sms/tags/1.9.14@708217 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-sms.php
+++ wp-sms.php
@@ -3,7 +3,7 @@
 Plugin Name: Wordpress SMS
 Plugin URI: http://wpbazar.com/plugins/wp-sms/
 Description: Send SMS from wordpress
-Version: 1.9.13
+Version: 1.9.14
 Author: Mostafa Soufi
 Author URI: URI: http://iran98.org/
 License: GPL2
