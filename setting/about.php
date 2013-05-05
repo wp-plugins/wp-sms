01a98eb56242ef2fdb78596a19488f5ea9f909c1
mostafa.s1990 <mostafa.s1990@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-05 12:45:31 +0000

    update new version.
    git-svn-id: https://plugins.svn.wordpress.org/wp-sms/tags/1.9.14@708197 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ setting/about.php
@@ -0,0 +1,19 @@
+<div class="wrap">
+	<h2><?php _e('About Plugin', 'wp-sms'); ?></h2>
+	<img src="<?php bloginfo('url'); ?>/wp-content/plugins/wp-sms/images/sms-128.png"/>
+	<p><?php echo sprintf(__('Version plugin: %s', 'wp-sms'), WP_SMS_VERSION); ?></p>
+	<p><?php echo sprintf(__('The first free WordPress Iranian plugin that works on Web service messages.', 'wp-sms'), 'http://www.webstudio.ir'); ?>.</p>
+	<p><?php echo sprintf(__('This plugin created by %s from %s and %s gorup', 'wp-sms'), '<a href="http://profiles.wordpress.org/mostafa.s1990/">Mostafa Soufi</a>', '<a href="http://wpbazar.com">WP Bazar</a>', '<a href="http://forum.wp-parsi.com">WP Parsi</a>'); ?>
+	<h3><?php _e('do you have problem with plugin?', 'wp-sms'); ?></h3>
+	<p><?php echo sprintf(__('if you have you can tell your problem in <a href="%s">forum</a>.', 'wp-sms'), 'http://forum.wp-parsi.com/'); ?>.</p>
+
+	<h3><?php _e('are you a translator?', 'wp-sms'); ?></h3>
+	<p></p>
+	<p><?php echo sprintf(__('please send your translation to %s.', 'wp-sms'), '<code>mst404@gmail.com</code>'); ?></p>
+
+	<h3><?php _e('you want to know about how to add your web service to this plugin?', 'wp-sms'); ?></h3>
+	<p></p>
+	<p><?php echo sprintf(__('to adding your web service please contact with %s.', 'wp-sms'), '<code>mst404@gmail.com</code>'); ?></p>
+	
+	<a href="http://ads.iran98.org/view/link/17" target="_blank" alt="ads-link" title="فروش ویژه! وب سرویس مخصوص افزونه پیامک وردپرس"><img src="http://ads.iran98.org/view/banner/17" alt="ads-img" title="فروش ویژه! وب سرویس مخصوص افزونه پیامک وردپرس"/></a>
+</div>
