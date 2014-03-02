31e49bbd859fd540a4dd9904e1abc2f543e24e19
mostafa.s1990 <mostafa.s1990@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-02 12:41:36 +0000

    Complate files.
    git-svn-id: https://plugins.svn.wordpress.org/wp-sms/tags/2.3.3@867973 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/templates/wp-sms-widget.php
@@ -0,0 +1,6 @@
+<p>
+	<?php _e('Name', 'wp-sms'); ?>:<br />
+	<input id="wp_sms_widget_name" name="wp_sms_widget_name" type="text" value="<?php echo get_option('wp_sms_widget_name'); ?>" />
+</p>
+
+<input type="hidden" id="wp_sms_submit_widget" name="wp_sms_submit_widget" value="1" />
