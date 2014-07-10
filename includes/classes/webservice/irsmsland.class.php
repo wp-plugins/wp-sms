c2e9254df1fad04f8a2895012211925111be2146
mostafa.s1990 <mostafa.s1990@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-10 12:25:30 +0000

    Complate files.
    git-svn-id: https://plugins.svn.wordpress.org/wp-sms/tags/2.5.1@946207 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/classes/webservice/irsmsland.class.php
@@ -0,0 +1,30 @@
+<?php
+	class irsmsland extends WP_SMS {
+		private $wsdl_link = "http://sms.irsmsland.ir/webservice/wsdl.wsdl";
+		public $tariff = "http://irsmsland.ir";
+		public $unitrial = false;
+		public $unit;
+		public $flash = "enable";
+		public $isflash = false;
+		
+		public function __construct() {
+			parent::__construct();
+		}
+		
+		public function SendSMS() {
+			$result = $client->send($this->username, $this->password, $this->to, $this->from, $this->msg);
+			
+			if($result) {
+				$this->InsertToDB($this->from, $this->msg, $this->to);
+				$this->Hook('wp_sms_send', $result);
+			}
+			
+			return $result;
+		}
+		
+		public function GetCredit() {
+			$client = new SoapClient($this->wsdl_link);
+			return $client->getCredit($this->username, $this->password);
+		}
+	}
+?>
