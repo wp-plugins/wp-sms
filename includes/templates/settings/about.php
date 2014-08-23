3f8d5c234b1395b0a11beace830a392a8fa3449d
mostafa.s1990 <mostafa.s1990@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-23 17:42:29 +0000

    - Tagged v2.5.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-sms/tags/2.5.3@971451 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/templates/settings/about.php
@@ -0,0 +1,28 @@
+<div class="wrap">
+	<h2><?php _e('About Plugin', 'wp-sms'); ?></h2>
+	<p><?php echo sprintf(__('Version plugin: %s', 'wp-sms'), WP_SMS_VERSION); ?></p>
+	<p><?php echo sprintf(__('The first free WordPress Iranian plugin that works on Web service messages.', 'wp-sms'), 'http://www.webstudio.ir'); ?></p>
+	<p><?php echo sprintf(__('This plugin created by %s from %s gorup', 'wp-sms'), '<a href="http://profiles.wordpress.org/mostafa.s1990/">Mostafa Soufi</a>', '<a href="http://forum.wp-parsi.com">WP Parsi</a>'); ?>
+	<h3><?php _e('do you have problem with plugin?', 'wp-sms'); ?></h3>
+	<p><?php echo sprintf(__('if you have you can tell your problem in <a href="%s">forum</a>.', 'wp-sms'), 'http://forum.wp-parsi.com/'); ?></p>
+
+	<h3><?php _e('are you a translator?', 'wp-sms'); ?></h3>
+	<p><?php echo sprintf(__('please send your translation to %s.', 'wp-sms'), '<code>mst404@gmail.com</code>'); ?></p>
+
+	<h3><?php _e('you want to know about how to add your web service to this plugin?', 'wp-sms'); ?></h3>
+	<p><?php echo sprintf(__('to adding your web service please contact with %s.', 'wp-sms'), '<code>mst404@gmail.com</code>'); ?></p>
+	
+	<h3><?php _e('Donate in plugin.', 'wp-sms'); ?></h3>
+	<p>
+		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="Z959U3RPCC9WG">
+			<input type="image" src="https://www.paypalsmsects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+			<img alt="" border="0" src="https://www.paypalsmsects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+		</form>
+		
+		<hr />
+		
+		<p><a href="http://www.mostafa-soufi.ir/donate/" target="_blank"><img src="<?php echo WP_SMS_DIR_PLUGIN; ?>assets/images/donate/donate.png" id="donate" alt="<?php _e('Donate', 'wp_statistics'); ?>"/><br /><img src="<?php echo WP_SMS_DIR_PLUGIN; ?>assets/images/donate/tdCflg.png" id="donate" alt="<?php _e('Donate', 'wp_statistics'); ?>"/></a></p>
+	</p>
+</div>
---
+++ includes/templates/wp-sms-tell-friend-head.php
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+	jQuery(document).ready(function(){
+		jQuery("#send_friend").click(function(){
+			jQuery("#tell_friend_form").slideToggle();
+		});
+	})
+</script>
