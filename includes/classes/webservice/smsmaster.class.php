b8ff84b9be3202758eda2b17c7115f6b5cd04cf3
mostafa.s1990 <mostafa.s1990@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-05 13:11:45 +0000

    - update readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/wp-sms/tags/2.5.4@981752 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/classes/webservice/smsmaster.class.php
@@ -0,0 +1,24 @@
+<?php
+	class smsmaster extends WP_SMS {
+		private $wsdl_link = "http://smsmaster.ir/send_webservice2.php";
+		public $tariff = "http://smsmaster.ir/";
+		public $unitrial = true;
+		public $unit;
+		public $flash = "disable";
+		public $isflash = false;
+
+		public function __construct() {
+			parent::__construct();
+			include_once dirname( __FILE__ ) . '/../nusoap.class.php';
+			$this->client = new nusoap_client($this->wsdl_link);
+		}
+
+		public function SendSMS() {
+			return $this->client->call('sending_sms',array($this->username, $this->password, $this->msg, implode('*', $this->to), 1, 0, $this->from));
+		}
+
+		public function GetCredit() {
+			return $this->client->call('getcredit', array($this->username, $this->password));
+		}
+	}
+?>
