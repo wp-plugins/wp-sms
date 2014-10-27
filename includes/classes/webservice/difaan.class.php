e7ddd0b69698559e5f0a5012a6e8e4fc2808ba16
mostafa.s1990 <mostafa.s1990@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-27 18:14:05 +0000

    - Added Difaan webservice.
    git-svn-id: https://plugins.svn.wordpress.org/wp-sms/trunk@1014830 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/classes/webservice/difaan.class.php
@@ -0,0 +1,34 @@
+<?php
+	class difaan extends WP_SMS {
+		private $wsdl_link = "http://119.152.247.218:9710/";
+		public $tariff = "http://119.152.247.218:9710/";
+		public $unitrial = false;
+		public $unit;
+		public $flash = "enable";
+		public $isflash = false;
+		
+		public function __construct() {
+			parent::__construct();
+		}
+		
+		public function SendSMS() {
+			
+			$msg = urlencode($this->msg);
+			
+			foreach($this->to as $number) {
+				$result = file_get_contents("{$this->wsdl_link}http/send-message?username={$this->username}&password={$this->password}&to={$number}&message-type=sms.automatic&message={$msg}");
+			}
+			
+			if ($result) {
+				$this->InsertToDB($this->from, $this->msg, $this->to);
+				$this->Hook('wp_sms_send', $result);
+			}
+			
+			return $result;
+		}
+		
+		public function GetCredit() {
+			return 1;
+		}
+	}
+?>
--- includes/templates/settings/web-service.php
+++ includes/templates/settings/web-service.php
@@ -279,6 +279,13 @@
 							</option>
 						</optgroup>
 						
+						<optgroup label="<?php _e('Pakistan', 'wp-sms'); ?>">
+							<option value="difaan" <?php selected(get_option('wp_webservice'), 'difaan'); ?>>
+								&nbsp;&nbsp;-&nbsp;
+								<?php echo sprintf(__('Web Service (%s)', 'wp-sms'), 'Difaan'); ?>
+							</option>
+						</optgroup>
+						
 						<!--Option information-->
 						<option value="1" id="option-information"><?php _e('For more information about adding Web Service', 'wp-sms'); ?></option>
 						<!--Option information-->
