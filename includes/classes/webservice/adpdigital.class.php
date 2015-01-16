ab437efb30915dba001d2fbf1228de2959b91606
mostafa.s1990 <mostafa.s1990@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-16 15:09:01 +0000

    - Tagged V2.7
    git-svn-id: https://plugins.svn.wordpress.org/wp-sms/tags/2.7@1069462 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/classes/webservice/adpdigital.class.php
@@ -0,0 +1,42 @@
+<?php
+	class adpdigital extends WP_SMS {
+		private $wsdl_link = "http://ws2.adpdigital.com/url/";
+		public $tariff = "http://adpdigital.com/services/";
+		public $unitrial = true;
+		public $unit;
+		public $flash = "disable";
+		public $isflash = false;
+
+		public function __construct() {
+			parent::__construct();
+			$this->validateNumber = "09xxxxxxxx";
+			
+			ini_set("soap.wsdl_cache_enabled", "0");
+		}
+
+		public function SendSMS() {
+			$to = str_replace("09", "989", implode($this->to, ","));
+			
+			$result = file_get_contents("{$this->wsdl_link}send?username={$this->username}&password={$this->password}&dstaddress={$to}&body={$this->msg}&clientid={$this->from}&type=text&unicode=1");
+			
+			if( strstr($result, 'ERR') ) {
+				return 0;
+			} else {
+				$this->InsertToDB($this->from, $this->msg, $this->to);
+				$this->Hook('wp_sms_send', $result);
+				
+				return preg_replace('/[^0-9]/', '', $result);
+			}
+		}
+
+		public function GetCredit() {
+			$result = file_get_contents("{$this->wsdl_link}balance?username={$this->username}&password={$this->password}&facility=send");
+			
+			if( strstr($result, 'ERR') ) {
+				return 0;
+			} else {
+				return preg_replace('/[^0-9]/', '', $result);
+			}
+		}
+	}
+?>
